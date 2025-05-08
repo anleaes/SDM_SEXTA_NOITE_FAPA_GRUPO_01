c3985fc65aa3fab2f9ebaf66899bd1ca26dcaa78
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2025-05-07 21:03:13 -0300

    Feat: adicionar a classe SocialNetworkViewSet

--- apps/socialnetworks/views.py
+++ apps/socialnetworks/views.py
@@ -3,3 +3,7 @@ from .models import SocialNetwork
 from rest_framework import viewsets
 from .serializer import SocialNetworkSerializer
 # Create your views here.
+
+class SocialNetworkViewSet(viewsets.ModelViewSet):
+    queryset = SocialNetwork.objects.all()
+    serializer_class = SocialNetworkSerializer  
