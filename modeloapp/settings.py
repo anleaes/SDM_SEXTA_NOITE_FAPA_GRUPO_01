d5452337ab58eac411a1f1964386d17fffdd337d
pedro-rohr <pedrorohr04@gmail.com>
2025-05-07 22:34:35 -0300

    Feat: finalização de setup dos pedidos

--- modeloapp/settings.py
+++ modeloapp/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'socialnetworks.apps.SocialNetworksConfig',
     'clients.apps.ClientsConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
